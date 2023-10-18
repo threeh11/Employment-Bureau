b4e8edf0ecd002816e26b615d7c4edfef481cf3c
threeh <threeh1@vk.com>
2023-10-18 22:52:20 +0300

    лучше такие данные из env брать)

--- internal/database/postgres/sqlhandler.go
+++ internal/database/postgres/sqlhandler.go
@@ -6,7 +6,7 @@ import (
 )
 
 func SetUpDb() *gorm.DB {
-	dsn := "host=localhost user=threeh password=asd890jkl dbname=employment_bureau port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host= user= password= dbname= port= sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
